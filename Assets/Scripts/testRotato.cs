fd03c54590c18e90b427a381cd7a9258fa4fe5d3
Cameron <chegney6@gmail.com>
2020-07-14 11:15:08 +1000

    Rotation Script

---
+++ Assets/Scripts/testRotato.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRotato : MonoBehaviour
+{
+    public ridgidbody rb3d;
+
+    void Awake()
+    {
+       
+    }
+
+    void Update()
+    {
+        
+    }
+}
