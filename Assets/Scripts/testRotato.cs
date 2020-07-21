2c532f4b3b84d1409d57b77e79bf924f499f1c6c
Cameron <chegney6@gmail.com>
2020-07-21 15:16:37 +1000

    Mouse Movement

--- Assets/Scripts/testRotato.cs
+++ Assets/Scripts/testRotato.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class testRotato : MonoBehaviour
 {
 
+    public Rigidbody rb3d;
     void Awake()
     {
-       
+        rb3d = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetAxis("Mouse X") < 0)
         {
+            rb3d.AddForce(-0.1f, 0f, 0f);
             //Code for action on mouse moving left
             print("Mouse moved left");
         }
         if (Input.GetAxis("Mouse X") > 0)
         {
+            rb3d.AddForce(0.1f, 0f, 0f);
             //Code for action on mouse moving right
             print("Mouse moved right");
         }
         if (Input.GetAxis("Mouse Y") < 0)
         {
+            rb3d.AddForce(0f, -0.1f, 0f);
             //Code for action on mouse moving left
             print("Mouse moved down");
         }
         if (Input.GetAxis("Mouse Y") > 0)
         {
+            rb3d.AddForce(0f, 0.1f, 0f);
             //Code for action on mouse moving right
             print("Mouse moved up");
         }
