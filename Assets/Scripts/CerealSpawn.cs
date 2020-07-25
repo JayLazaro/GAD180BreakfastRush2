3b6f0ab1b56f9ebf997aae9a9b0a9ed68fbb8bdd
Daniel Butcher <1020078@student.sae.edu.au>
2020-07-25 13:33:12 +1000

    Cereal Spawn and spawn point

---
+++ Assets/Scripts/CerealSpawn.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CerealSpawn : MonoBehaviour
+{
+
+    public GameObject SpawnPoint;
+
+    void Start()
+        
+
+    {
+        StartCoroutine(coroutine());
+    
+    }
+
+    
+    IEnumerator coroutine(){
+       
+
+
+            GameObject prefab = Resources.Load("Cube") as GameObject;
+            for (int i = 0; i < 200; i++)
+            {
+                GameObject go = Instantiate(prefab) as GameObject;
+                go.transform.position = GameObject.Find("SpawnPoint").transform.position;
+            yield return new WaitForSeconds(.1f);
+        }
+        
+
+    }
+
+
+
+
+
+
+
+    void Update()
+    {
+      
+    }
+}
