fbebee609e50d502359ba770cdc018064c235537
Jaylaan Lazaro <1019562@student.sae.edu.au>
2020-07-28 13:34:23 +1000

    Scoring System and UI Implementation
    Scoring System works fine
    The UI cannot convert the int to string (NEEDS WORK)

---
+++ Assets/Scripts/Detection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Detection : MonoBehaviour
+{
+    
+    public int GameScore = 0;
+    public GameObject DetectionBox;
+
+    void Start()
+    {
+       
+
+    }
+
+    void Update()
+    {
+        
+
+    }
+
+    private void OnTriggerEnter()
+    {
+       print("Something is entering the box");
+       GameScore++;
+    }
+
+    private void OnTriggerExit()
+    {
+       print("Something is exiting the box");
+       GameScore--;
+       
+    }
+
+    // Update is called once per frame
+    
+
+}
