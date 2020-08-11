5eed37045954730dd9f89ac25ee698ac73d4e018
Daniel Butcher <1020078@student.sae.edu.au>
2020-08-11 14:39:41 +1000

    Waiting for slide

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) //key"E" is used to load the environment 'maytheforth' and score script with score value set at 0
+        {
+
+            Application.LoadLevel("controller");
+           
+        }
+
+    }
+}
